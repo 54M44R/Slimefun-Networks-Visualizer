dc500374f5f9ef6a5de21d04a7ecb3380f8ec995
54M44R <26176694+54M44R@users.noreply.github.com>
2022-04-13 02:19:46 -0400

    Improvements and tools
    - Added GUI element textures
    - Added tools to convert between light and dark UI elements
    - Added texture for Network Monitor
    - Pack.mcmeta format change (no longer using dates as that is terrible)

---
+++ Converter & Assembler/Convert.bat
@@ -0,0 +1,37 @@
+2@echo off
+:start
+cls
+echo --------------------------------------
+echo. 
+echo          GUI Conversion Tool
+echo. 
+echo --------------------------------------
+echo.
+echo     Light(1)    Dark(2)     Exit(3)
+echo.
+set /p Input=Selection: 
+if /I "%Input%" == "1" goto light
+if /I "%Input%" == "2" goto dark
+if /I "%Input%" == "3" goto exit
+if /I "%Input%" == "exit" goto exit
+goto error
+
+
+:light
+copy Light\*.json ..\Slimefun-Networks-Visualizer\assets\minecraft\optifine\cit\gui
+goto start
+
+
+:dark
+copy Dark\*.json ..\Slimefun-Networks-Visualizer\assets\minecraft\optifine\cit\gui
+goto start
+
+
+:error
+echo %Input% is not a valid option
+pause
+goto :start
+
+
+:exit
+exit
